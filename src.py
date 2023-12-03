f3bcc2cdb46855e956b70d8a789f3dfc82a8e3d6
Haoran Yin <h.yin@liacs.leidenuniv.nl>
2023-12-03 17:21:38 +0100

    draft

--- src.py
+++ src.py
@@ -1,27 +1,33 @@
 import ioh
 import argparse
 import numpy as np
-from tqdm import trange
 from modde import ModularDE
 
 
 class LSHADE_interface():
-    def __init__(self, bound_corr):
+    def __init__(self, bound_corr, r_N_init, r_arc, p, H, budget):
         self.bound_corr = bound_corr
+        self.r_N_init = r_N_init
+        self.r_arc = r_arc
+        self.p = p
+        self.H = H
+        self.budget = budget
         self.lshade = None
 
     def __call__(self, f):
-        self.lshade = ModularDE(f, base_sampler='uniform',
+        lambda_ = self.r_N_init*f.meta_data.n_variables
+        archive_size = int(self.r_arc*self.r_N_init*f.meta_data.n_variables)
+        self.lshade = ModularDE(f, budget=budget, base_sampler='uniform',
                                 mutation_base='target',
                                 mutation_reference='pbest',
                                 bound_correction=self.bound_corr,
                                 crossover='bin', lpsr=True,
-                                lambda_=18*f.meta_data.n_variables,
-                                memory_size=6, use_archive=True,
-                                archive_size=int(2.6*18*f.meta_data.n_variables),
+                                lambda_=lambda_,
+                                memory_size=self.H, use_archive=True,
+                                archive_size=archive_size,
                                 init_stats=True, adaptation_method_F='shade',
                                 adaptation_method_CR='shade',
-                                pbest_value=0.11)
+                                pbest_value=self.p)
         self.lshade.run()
 
     @property
@@ -63,16 +69,22 @@ class LSHADE_interface():
 
 if __name__ == "__main__":
     # argparser
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument('-n', '--near_bounds',
                         help='Determine how many dimesions of xopt are near to \
                             the boundaries', required=True)
     parser.add_argument('-i', '--index_params',
                         help='Indicate which set of weights and iids to be \
                             used when generating MA-BBOB', required=True)
+    parser.add_argument('-b', '--bound_corr',
+                        help='How to deal with the box-constraints. Choices: \
+                            None, \"saturate\", \"unif_resample\", \"COTN\", \
+                            \"toroidal\", \"mirror\", \"hvb\", \"expc_target\",\
+                            \"expc_center\", \"exps\".', required=True)
     args = vars(parser.parse_args())
-    near_bounds = int(args['near_bounds'])
-    index_params = int(args['index_params'])
+    number_of_dims_near_boundaries = int(args['near_bounds'])
+    index_of_params_generating_problem = int(args['index_params'])
+    boundary_correction = None if args['bound_corr'] == 'None' else args['bound_corr']
     # load xopt, weights, and iids for ioh.problem.ManyAffine
     xopts = np.loadtxt("data/xopts_20.txt")
     weights = np.loadtxt("data/weights.txt")
@@ -82,23 +94,44 @@ if __name__ == "__main__":
     ndim = xopts.shape[1]
     nparams = weights.shape[0]
     budget = 5000 * ndim
+    r_N_init = 18
+    r_arc = 2.6
+    p = 0.11
+    H = 6
     runs = 25
     # generate problems
-    xopt_index = (near_bounds-1)*nparams+index_params
+    xopt_index = (number_of_dims_near_boundaries) * nparams + \
+        index_of_params_generating_problem
     problem = ioh.problem.ManyAffine(
-        xopts[xopt_index], weights[index_params], iids[index_params], ndim)
+        xopts[xopt_index],
+        weights[index_of_params_generating_problem],
+        iids[index_of_params_generating_problem],
+        ndim)
+    # problem.enforce_bounds(
+    #     how=ioh.ConstraintEnforcement.SOFT,
+    #     weight=1.0,
+    #     exponent=1.0
+    # )
     # experiment
-    print(problem.meta_data)
-    obj = LSHADE_interface('saturate')
-    exp = ioh.Experiment(algorithm=obj, fids=[1], iids=[1], dims=[20], reps=25,
+    obj = LSHADE_interface('saturate', r_N_init, r_arc, p, H, budget)
+    exp = ioh.Experiment(algorithm=obj,
+                         fids=[1], iids=[1], dims=[20], reps=runs,
                          problem_class=ioh.ProblemClass.REAL,
-                         njobs=12, logger_triggers=[ioh.logger.trigger.ALWAYS],
-                         logged=True, folder_name=f'L-SHADE_sat',
-                         algorithm_name=f'L-SHADE', store_positions=True,
+                         njobs=12,
+                         logged=True,
+                         logger_triggers=[
+                             ioh.logger.trigger.ALWAYS,
+                             ioh.logger.trigger.ON_VIOLATION],
+                         output_directory="./",
+                         folder_name=f'L-SHADE_sat',
+                         algorithm_name=f'L-SHADE',
+                         store_positions=True,
                          experiment_attributes={'SDIS': 'Saturate'},
-                         logged_attributes=['corrected', 'cumulative_corrected',
-                                            'F', 'CR', 'CS', 'ED'],
+                         logged_attributes=[
+                             'corrected', 'cumulative_corrected',
+                             'F', 'CR', 'CS', 'ED'],
                          merge_output=True, zip_output=True, remove_data=True)
     exp.add_custom_problem(
-        problem, "MA-BBOB_{}_{}".format(near_bounds, index_params))
+        problem, "MA-BBOB_{}_{}".format(number_of_dims_near_boundaries,
+                                        index_of_params_generating_problem))
     exp()
