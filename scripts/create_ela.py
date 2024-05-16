e5d1c8d379a7e92a87b6f1ecd5da9c5e11c1151f
Haoran Yin <h.yin@liacs.leidenuniv.nl>
2024-05-16 02:28:20 +0200

    ela calculated

--- scripts/create_ela.py
+++ scripts/create_ela.py
@@ -4,100 +4,68 @@ import time
 import numpy as np
 import pandas as pd
 
-from pflacco.sampling import create_initial_sample
+from pyDOE import lhs
 from pflacco.classical_ela_features import calculate_dispersion
 from pflacco.classical_ela_features import calculate_ela_distribution
 from pflacco.classical_ela_features import calculate_ela_level
 from pflacco.classical_ela_features import calculate_ela_meta
 from pflacco.classical_ela_features import calculate_information_content
-from pflacco.classical_ela_features import calculate_limo
 from pflacco.classical_ela_features import calculate_nbc
 from pflacco.classical_ela_features import calculate_pca
 
 
-def load_problems():
-    problems = []
-    iids = np.loadtxt("data/iids.txt").astype(np.int32)
-    weights = np.loadtxt("data/weights.txt")
-    xopts = np.loadtxt("data/xopts_20.txt")
-    nprob = iids.shape[0]
-    for xopt_index in range(xopts.shape[0]):
-        problem = ioh.problem.ManyAffine(
-            xopts[xopt_index],
-            weights[xopt_index % nprob],
-            iids[xopt_index % nprob], 20)
-        problems += [problem]
-    return problems
-
-
-def calculate_features(X, y, target):
+def calculate_features(X, y):
     keys = []
     values = []
-    if target == "disp":
-        disp = calculate_dispersion(X, y)
-        keys += list(disp.keys())[:-1]
-        values += list(disp.values())[:-1]
-    elif target == "ela_distr":
-        ela_distr = calculate_ela_distribution(X, y)
-        keys += list(ela_distr.keys())[:-1]
-        values += list(ela_distr.values())[:-1]
-    elif target == "ela_level":
-        ela_level = calculate_ela_level(X, y)
-        keys += list(ela_level.keys())[:-1]
-        values += list(ela_level.values())[:-1]
-    elif target == "ela_meta":
-        ela_meta = calculate_ela_meta(X, y)
-        keys += list(ela_meta.keys())[:-1]
-        values += list(ela_meta.values())[:-1]
-    elif target == "ic":
-        ic = calculate_information_content(X, y)
-        keys += list(ic.keys())[:-1]
-        values += list(ic.values())[:-1]
-    elif target == "nbc":
-        nbc = calculate_nbc(X, y)
-        keys += list(nbc.keys())[:-1]
-        values += list(nbc.values())[:-1]
-    elif target == "pca":
-        pca = calculate_pca(X, y)
-        keys += list(pca.keys())[:-1]
-        values += list(pca.values())[:-1]
+    disp = calculate_dispersion(X, y)
+    keys += list(disp.keys())[:-1]
+    values += list(disp.values())[:-1]
+    ela_distr = calculate_ela_distribution(X, y)
+    keys += list(ela_distr.keys())[:-1]
+    values += list(ela_distr.values())[:-1]
+    ela_level = calculate_ela_level(X, y)
+    keys += list(ela_level.keys())[:-1]
+    values += list(ela_level.values())[:-1]
+    ela_meta = calculate_ela_meta(X, y)
+    keys += list(ela_meta.keys())[:-1]
+    values += list(ela_meta.values())[:-1]
+    ic = calculate_information_content(X, y)
+    keys += list(ic.keys())[:-1]
+    values += list(ic.values())[:-1]
+    nbc = calculate_nbc(X, y)
+    keys += list(nbc.keys())[:-1]
+    values += list(nbc.values())[:-1]
+    pca = calculate_pca(X, y)
+    keys += list(pca.keys())[:-1]
+    values += list(pca.values())[:-1]
     return keys, values
 
 
+def ela_experiment(prob, epsilon):
+    if epsilon != 0.0:
+        bounds = np.loadtxt(f"data/search_region/{problem_id}_1_{epsilon}.txt")
+        prob.bounds.lb = bounds[:, 0]
+        prob.bounds.ub = bounds[:, 1]
+    lhs_matrix = lhs(20, samples=10000)
+    min_values = prob.bounds.lb
+    max_values = prob.bounds.ub
+    scaled_samples = np.zeros_like(lhs_matrix)
+    for i in range(20):
+        scaled_samples[:, i] = lhs_matrix[:, i] * \
+            (max_values[i] - min_values[i]) + min_values[i]
+    return calculate_features(scaled_samples, prob(scaled_samples))
+
+
 if __name__ == "__main__":
-    if not os.path.exists("data/samplingX.npy"):
-        samplingX = []
-        for _ in range(100):
-            X = create_initial_sample(20, lower_bound=-5, upper_bound=5)
-            samplingX += X.values.tolist()
-        samplingX = np.array(samplingX)
-        np.save("data/samplingX.npy", samplingX)
-    X = np.load("data/samplingX.npy")
-    X = pd.DataFrame(X)
-    problems = load_problems()
-    dataset_keys = []
+    ndim = 20
     dataset_values = []
-    for bounds in range(1, 21):
-        for pid in range(50):
-            start_time = time.time()
-            prob = problems[bounds*50+pid]
-            y = X.apply(lambda x: prob(x), axis=1)
-            y_norm = (y - y.min()) / (y.max() - y.min())
-            for sampling in range(100):
-                set_keys = ["pid", "iteration"]
-                set_values = [pid, sampling]
-                for feature in ["disp", "ela_distr", "ela_level", "ela_meta",
-                                "ic", "nbc", "pca"]:
-                    keys, values = calculate_features(
-                        X[1000*sampling:1000*sampling+1000],
-                        y[1000*sampling:1000*sampling+1000], feature)
-                    set_keys += keys
-                    set_values += values
-                if dataset_keys == []:
-                    dataset_keys = set_keys
-                dataset_values += [set_values]
-            end_time = time.time()
-            print(
-                f"Processing problem {pid} (boundaries {bounds}) in {end_time-start_time}s")
-        df = pd.DataFrame(dataset_values, columns=dataset_keys)
-        df.to_csv(f"data/ELA_{bounds}.csv", index=False)
+    epsilons = [0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.5, 0.0]
+    for problem_id in [1, 16, 23]:
+        for epsilon in epsilons:
+            print(f"Problem {problem_id}, epsilon {epsilon}")
+            prob = ioh.get_problem(problem_id, 1, ndim, ioh.ProblemClass.BBOB)
+            keys, values = ela_experiment(prob, epsilon)
+            dataset_values += [[problem_id, epsilon] + values]
+    dataset_keys = ["problem_id", "epsilon"] + keys
+    dataset = pd.DataFrame(dataset_values, columns=dataset_keys)
+    dataset.to_csv("data/ela_features.csv", index=False)
